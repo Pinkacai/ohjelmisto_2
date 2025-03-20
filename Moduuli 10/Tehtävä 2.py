12beee9c5e554b73a7729caaec20247f9081dad7
Pinkacai <eliel.j.levine@gmail.com>
2025-03-20 14:17:37 +0200

    IDK

--- Moduuli 10/Tehtävä 2.py
+++ Moduuli 10/Tehtävä 2.py
@@ -5,14 +5,15 @@ class Elevator:
         self.location = 0
 
     def go_to_floor(self,floor):
+        print(f"Elevator number{self}")
         print("Going up!")
-        while self.location <= floor:
+        while self.location < floor:
             self.floor_up()
-            print(f"{elevator_num} is at floor: {self.location}")
+            print(f"{self} is at floor: {self.location}")
         print("Going down!")
         while self.location > 0:
             self.floor_down()
-            print(f"{elevator_num} is at floor: {self.location}")
+            print(f"{self} is at floor: {self.location}")
 
     def floor_up(self):
         self.location = self.location + 1
@@ -26,23 +27,19 @@ class Building:
         self.top_floor = top_floor
         self.amt_of_elevators = amt_of_elevators
         self.elevators = []
+
         for i in range(1,amt_of_elevators + 1):
-            self.elevator_num = f"Elevator no.{i}"
-            self.elevators.append(self.elevator_num)
+            self.elevators.append(Elevator(self.bottom_floor,self.top_floor))
 
     def run_elevator(self,elevator_number,floor):
-        elevator_number = Elevator(self.bottom_floor,self.top_floor)
-        if flo <= b.top_floor:
-            elevator_number.go_to_floor(flo-1)
-        else:
-            print("That floor does not exist!")
+            self.elevators[elevator_number].go_to_floor(floor)
 
-b = Building(0,10,6)
-print("")
 flo = int(input("Which floor would you like to go to?: "))
+b = Building(0,10,6)
 
-for i in range(1,b.amt_of_elevators+1):
-    elevator_num = f"Elevator no.{i}"
-print(b.elevators)
-b.run_elevator(3,5)
+b.run_elevator(3,flo)
+print("")
+
+
+
 
