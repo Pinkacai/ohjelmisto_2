34739faebfa9b5343c517754e227326e9e1e3a4d
Pinkacai <eliel.j.levine@gmail.com>
2025-04-09 13:10:43 +0300

    IDK

--- Moduuli 12/Tehtävä 2.py
+++ Moduuli 12/Tehtävä 2.py
@@ -5,9 +5,9 @@ city_name = input("Please input the name of the city you wish to see the weather
 request = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=6f2c05652d50ce1b8ddeaeb5c2080c6f&units=metric"
 
 response = requests.get(request).json()
-
 print(" ")
 desc = (response['weather'][0]['description'])
 temp = (response['main']['temp'])
-print(f"It is currently {temp} degrees, with {desc} in {city_name}.")
+print(f"It is currently {temp} degrees Celcius, with {desc} in {city_name}.")
+
 
